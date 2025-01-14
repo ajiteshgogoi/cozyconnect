fd243d8c4634d14279b49449e74053aa7d263aec
ajitesh gogoi <136594063+ajiteshgogoi@users.noreply.github.com>
2025-01-14 09:27:48 +0530

    Fix local environment

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
     ? 'https://easyconnect-red.vercel.app'
-    : '*',
+    : 'http://localhost:3000',
   credentials: true,
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
